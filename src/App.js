27c063fec55ad61b574d91135e7ed9e1c9311a8e
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2023-06-02 04:41:15 +0100

    Updated App.js
    Added LoadingPage.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from 'react';
 import SPlashScreen from './Pages/SPlashScreen';
 import ChatPage from './Pages/ChatPage';
+import LoadingPage from './Pages/LoadingPage';
 
 export const appContext = createContext();
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     return (
         <appContext.Provider value={{splashState, setSplashState}}>
             <div className="app">
+                <LoadingPage />
                 <SPlashScreen />
                 <ChatPage />
             </div>
