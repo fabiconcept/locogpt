b7855fa61a2477ffc71f808dc3859ba64e3a4676
Fabian Ajokubi <70838932+fabiconcept@users.noreply.github.com>
2023-06-06 21:47:50 +0100

    added TTS

--- src/Pages/Chat components/ChatBox.jsx
+++ src/Pages/Chat components/ChatBox.jsx
@@ -1,20 +1,45 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import Bubble from './Bubble'
-import { chatPageContaxt } from '../ChatPage'
+import React, { useContext, useEffect, useRef } from 'react';
+import Bubble from './Bubble';
+import { chatPageContaxt } from '../ChatPage';
+import { useState } from 'react';
 
 const ChatBox = () => {
     const { chats, assistantThinking, I_Am_Typing } = useContext(chatPageContaxt);
-
+    const msg = new SpeechSynthesisUtterance();
     const divRef = useRef(null);
 
-  useEffect(() => {
-    divRef.current.scrollTop = divRef.current.scrollHeight;
-  }, [chats]);
+    const speechHandler = (msg, value) => {
+        window.speechSynthesis.cancel();
+        msg.text = value;
+        window.speechSynthesis.speak(msg);
+    }
+
+    const calculateTimeDifference = (timestamp) => {
+        const currentTime = new Date().getTime();
+        const givenTime = new Date(timestamp).getTime();
+        const differenceInSeconds = Math.floor((currentTime - givenTime) / 1000);
+        return differenceInSeconds;
+    };
+
+    useEffect(() => {
+        divRef.current.scrollTop = divRef.current.scrollHeight;
+
+        if (chats.length > 0) {
+            const lastMessage = chats[chats.length - 1];
+            const lastMessageValue = lastMessage.value;
+            const lastMessageType = lastMessage.type;
+            if (lastMessageType === 0 && calculateTimeDifference(lastMessage.timestamp) < 5) {
+                speechHandler(msg, lastMessageValue);
+            }else{
+                window.speechSynthesis.cancel();
+            }
+        }
+    }, [chats]);
 
     return (
         <div className="chatSection" ref={divRef}>
-            {chats.map(i=>(
-                <Bubble text={i.value} timestamp={i.timestamp} type={i.type} key={i.key}/>
+            {chats.map(i => (
+                <Bubble text={i.value} timestamp={i.timestamp} type={i.type} key={i.key} />
             ))}
             {assistantThinking && <div className="pending bubble">
                 <div className="ellipsis">
@@ -30,11 +55,9 @@ const ChatBox = () => {
                     <div></div>
                 </div>
             </div>}
+
         </div>
     )
 }
 
-export default ChatBox
-            // <div className="timeMaker">
-            //     <span>Today</span>
-            // </div>
+export default ChatBox;
